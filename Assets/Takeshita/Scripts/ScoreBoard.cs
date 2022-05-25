112c3aa0c467a466557fa59d4718bdf5354bca74
ChihiroTakeshita <83819925+ChihiroTakeshita@users.noreply.github.com>
2022-05-25 17:18:54 +0900

    ゲーム終了時のスコアボードを作成しました

---
+++ Assets/Takeshita/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] Text _score;
+    [SerializeField] Text _highScore;
+
+    private void OnEnable()
+    {
+        _score.text = GameManager.KilledEnemy.ToString();
+        _highScore.text = GameManager.HighScore.ToString();
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("タイトル画面へ");
+        //SceneManager.LoadScene(0);
+    }
+}
